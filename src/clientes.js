78e1cdbcce9f47c19eb2c2525054c9ac6aedaaaf
thaissthephanye <thaissthephanye@gmail.com>
2025-03-20 21:12:49 -0300

    page obras ajustada

--- src/clientes.js
+++ src/clientes.js
@@ -4,27 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnSalvar = document.getElementById("btnSalvar");
   const tbodyClientes = document.getElementById("tbodyClientes");
 
-  // Função para carregar dados do localStorage ou usar o dataset inicial
   function loadLocalStorage() {
     let clientes = localStorage.getItem("clientes");
     if (!clientes) {
-      // Se não houver dados no localStorage, usa o clientesDataset
       clientes = clientesDataset;
-      localStorage.setItem("clientes", JSON.stringify(clientes)); // Salva o dataset no localStorage
+      localStorage.setItem("clientes", JSON.stringify(clientes));
     } else {
-      clientes = JSON.parse(clientes); // Converte a string JSON para um array
+      clientes = JSON.parse(clientes);
     }
     return clientes;
   }
 
-  // Função para salvar dados no localStorage
   function saveToLocalStorage(clientes) {
     localStorage.setItem("clientes", JSON.stringify(clientes));
   }
 
-  // Função para renderizar a tabela com os dados
   function renderTable(clientes) {
-    tbodyClientes.innerHTML = ""; // Limpa o conteúdo atual da tabela
+    tbodyClientes.innerHTML = "";
 
     clientes.forEach((cliente) => {
       const row = `
@@ -39,20 +35,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Carregar dados do localStorage (ou dataset) e renderizar a tabela ao carregar a página
   let clientes = loadLocalStorage();
   renderTable(clientes);
 
-  // Adicionar evento de clique ao botão "Salvar"
   btnSalvar.addEventListener("click", function () {
-    // Capturar os valores dos inputs
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const cpf = document.getElementById("cpf").value;
 
-    // Validar se os campos estão preenchidos
     if (nome && email && cpf) {
-      // Criar um novo objeto cliente
       const novoCliente = {
         id: (clientes.length + 1).toString(), // Gera um ID único
         nome: nome,
@@ -60,19 +51,14 @@ document.addEventListener("DOMContentLoaded", function () {
         cpf: cpf,
       };
 
-      // Adicionar o novo cliente ao array
       clientes.push(novoCliente);
 
-      // Salvar no localStorage
       saveToLocalStorage(clientes);
 
-      // Renderizar a tabela novamente
       renderTable(clientes);
 
-      // Limpar o formulário
       document.getElementById("formUsuario").reset();
 
-      // Fechar o modal
       const modal = bootstrap.Modal.getInstance(
         document.getElementById("exampleModal")
       );
--- src/obras.js
+++ src/obras.js
@@ -1,93 +1,88 @@
 import { obrasDataset } from "./dataset.js";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const btnSalvar = document.getElementById("btnSalvar");
-  const tbodyObras = document.getElementById("tbodyObras");
-
-  // Função para carregar dados do localStorage ou usar o dataset inicial
-  function loadLocalStorage() {
-    let obras = localStorage.getItem("obras");
-    if (!obras) {
-      // Se não houver dados no localStorage, usa o obrasDataset
-      obras = obrasDataset;
-      localStorage.setItem("obras", JSON.stringify(obras)); // Salva o dataset no localStorage
-    } else {
-      obras = JSON.parse(obras); // Converte a string JSON para um array
-    }
-    return obras;
+function loadLocalStorage() {
+  let obrasLocalStorage = localStorage.getItem("obras");
+  if (obrasLocalStorage == null) {
+    localStorage.setItem("obras", JSON.stringify(obrasDataset));
+    obrasLocalStorage = localStorage.getItem("obras");
   }
+  return JSON.parse(obrasLocalStorage);
+}
 
-  // Função para salvar dados no localStorage
-  function saveToLocalStorage(obras) {
-    localStorage.setItem("obras", JSON.stringify(obras));
-  }
+const loadObrasTable = () => {
+  let tbodyObras = document.getElementById("tbodyObras");
+  tbodyObras.innerHTML = "";
 
-  // Função para renderizar a tabela com os dados
-  function renderTable(obras) {
-    tbodyObras.innerHTML = ""; // Limpa o conteúdo atual da tabela
-
-    obras.forEach((obra) => {
-      const row = `
-        <tr>
-          <td>${obra.id}</td> <!-- Coluna do ID -->
-          <td>${obra.nome}</td>
-          <td>${obra.tipoConstrucao}</td>
-          <td>${obra.logradouro}</td>
-          <td>${obra.bairro}</td>
-          <td>${obra.cidade}</td>
-          <td>${obra.estado}</td>
-        </tr>
-      `;
-      tbodyObras.insertAdjacentHTML("beforeend", row);
-    });
-  }
-
-  // Carregar dados do localStorage (ou dataset) e renderizar a tabela ao carregar a página
   let obras = loadLocalStorage();
-  renderTable(obras);
+  for (const obra of obras) {
+    tbodyObras.insertAdjacentHTML("beforeend", getRowObraTable(obra));
+  }
+};
 
-  // Adicionar evento de clique ao botão "Salvar"
-  btnSalvar.addEventListener("click", function () {
-    // Capturar os valores dos inputs
-    const nome = document.getElementById("nome").value;
-    const tipoConstrucao = document.getElementById("tipoConstrucao").value;
-    const logradouro = document.getElementById("logradouro").value;
-    const bairro = document.getElementById("bairro").value;
-    const cidade = document.getElementById("cidade").value;
-    const estado = document.getElementById("estado").value;
+const getRowObraTable = (obra) => {
+  return `<tr>
+    <td>${obra.id}</td>
+    <td>${obra.nome}</td>
+    <td>${obra.tipoConstrucao}</td>
+    <td>${obra.logradouro}</td>
+    <td>${obra.bairro}</td>
+    <td>${obra.cidade}</td>
+    <td>${obra.estado}</td>
+  </tr>`;
+};
 
-    // Validar se os campos estão preenchidos
-    if (nome && tipoConstrucao && logradouro && bairro && cidade && estado) {
-      // Criar um novo objeto obra
-      const novaObra = {
-        id: (obras.length + 1).toString(), // Gera um ID único
-        nome: nome,
-        tipoConstrucao: tipoConstrucao,
-        logradouro: logradouro,
-        bairro: bairro,
-        cidade: cidade,
-        estado: estado,
-      };
+let obraForm = document.getElementById("formObras");
+let btnSalvar = document.getElementById("btnSalvar");
 
-      // Adicionar a nova obra ao array
-      obras.push(novaObra);
+btnSalvar.addEventListener("click", (event) => {
+  event.preventDefault();
 
-      // Salvar no localStorage
-      saveToLocalStorage(obras);
+  let nome = document.getElementById("nome").value;
+  let tipoConstrucao = document.getElementById("tipoConstrucao").value;
+  let logradouro = document.getElementById("logradouro").value;
+  let bairro = document.getElementById("bairro").value;
+  let cidade = document.getElementById("cidade").value;
+  let estado = document.getElementById("estado").value;
 
-      // Renderizar a tabela novamente
-      renderTable(obras);
+  if (nome && tipoConstrucao && logradouro && bairro && cidade && estado) {
+    let novaObra = {
+      id: (loadLocalStorage().length + 1).toString(), // Gera um ID único
+      nome: nome,
+      tipoConstrucao: tipoConstrucao,
+      logradouro: logradouro,
+      bairro: bairro,
+      cidade: cidade,
+      estado: estado,
+    };
 
-      // Limpar o formulário
-      document.getElementById("formObras").reset();
+    let obras = loadLocalStorage();
+    obras.push(novaObra);
 
-      // Fechar o modal
-      const modal = bootstrap.Modal.getInstance(
-        document.getElementById("exampleModal")
-      );
-      modal.hide();
-    } else {
-      alert("Por favor, preencha todos os campos!");
-    }
-  });
+    localStorage.setItem("obras", JSON.stringify(obras));
+
+    loadObrasTable();
+
+    Toastify({
+      text: "Obra cadastrada com sucesso!",
+      className: "success",
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      backgroundColor:
+        "linear-gradient(to right,rgb(10, 27, 177),rgb(61, 110, 201))",
+    }).showToast();
+
+    // Fecha o modal
+    let modal = bootstrap.Modal.getInstance(
+      document.getElementById("exampleModal")
+    );
+    modal.hide();
+
+    obraForm.reset();
+  } else {
+    alert("Por favor, preencha todos os campos!");
+  }
 });
+
+loadObrasTable();
